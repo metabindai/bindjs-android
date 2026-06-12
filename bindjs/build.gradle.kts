20fe7d12c7ed9c6654603a1e0c0d74e3a1f5642f
Misha <misha@velosmobile.com>
2026-06-12 12:51:48 -0500

    Bump bindjs-android to 0.0.16

--- bindjs/build.gradle.kts
+++ bindjs/build.gradle.kts
@@ -47,7 +47,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "ai.metabind"
             artifactId = "bindjs-android"
-            version = "0.0.15"
+            version = "0.0.16"
 
             afterEvaluate {
                 from(components["default"])
